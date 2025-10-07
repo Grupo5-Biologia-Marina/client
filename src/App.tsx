9cb8cab53268328f74b771b6e3cd0ffafc5ab85f
irinatiron <irinatirongiurgiu@gmail.com>
2025-10-07 16:12:33 +0200

    fix: footer on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./router/router";
 import "./styles/index.css";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 }
