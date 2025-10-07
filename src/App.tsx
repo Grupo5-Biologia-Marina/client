9232fb053ee81b453dd018db079b23dbad494b57
irinatiron <irinatirongiurgiu@gmail.com>
2025-10-07 14:41:16 +0200

    feat(routes): add client-side routes based on backend endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import WelcomePage from "./pages/WelcomePage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import DiscoveriesPage from "./pages/DiscoveriesPage";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./router/router";
 import "./styles/index.css";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/discoveries" element={<DiscoveriesPage />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
---
+++ src/router/router.tsx
@@ -0,0 +1,42 @@
+import { Routes, Route } from "react-router-dom";
+
+// Auth
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+
+// Users
+import ProfilePage from "../pages/ProfilePage"     // GET /users/:id
+import UsersAdminPage from "../pages/UsersAdminPage"; // GET /users (admin)
+
+// Posts
+import WelcomePage from "../pages/WelcomePage";        
+import DiscoveriesPage from "../pages/DiscoveriesPage"; // GET /posts   
+import PostDetailPage from "../pages/PostDetailPage"; // GET /posts/:id
+import CreatePostPage from "../pages/CreatePostPage"; // POST /posts
+
+// Common
+import NotFoundPage from "../pages/NotFoundPage";
+
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      {/* Auth */}
+      <Route path="/login" element={<LoginPage />} />          {/* POST /auth/login */}
+      <Route path="/register" element={<RegisterPage />} />    {/* POST /auth/register */}
+
+      {/* Posts */}
+      <Route path="/" element={<WelcomePage />} />         
+      <Route path="/discoveries" element={<DiscoveriesPage />} /> {/* GET /posts */}    
+      <Route path="/posts/:id" element={<PostDetailPage />} /> {/* GET /posts/:id */}
+      <Route path="/posts/new" element={<CreatePostPage />} /> {/* POST /posts */}
+
+      {/* Users */}
+      <Route path="/users/:id" element={<ProfilePage />} />    {/* GET /users/:id */}
+      <Route path="/admin/users" element={<UsersAdminPage />} /> {/* GET /users */}
+
+      {/* Fallback */}
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+}
