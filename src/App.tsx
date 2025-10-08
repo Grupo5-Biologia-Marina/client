9a7230ccffc9f9133f5add936792ed7effc73455
Aday25 <aday.alvarez84@gmail.com>
2025-10-08 09:37:56 +0200

    some changes applicated on navbar and app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import AppRoutes from "./router/router";
 import "./styles/index.css";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { useState } from "react";
+
+function AppContent() {
+  const location = useLocation();
+  const [username, setUsername] = useState("User"); // O traer del contexto/auth
+
+  const hideNavFooter = ["/welcome", "/login", "/register"];
+  const shouldHide = hideNavFooter.includes(location.pathname);
+
+  const handleLogout = () => {
+    console.log("Cerrar sesión");
+    // Aquí borras token, rediriges, etc.
+  };
+
+  return (
+    <>
+      {!shouldHide && <Navbar username={username} onLogout={handleLogout} />}
+      <AppRoutes />
+      {!shouldHide && <Footer />}
+    </>
+  );
+}
 
 export default function App() {
   return (
-    <>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-      <Footer />
-    </>
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
   );
 }
